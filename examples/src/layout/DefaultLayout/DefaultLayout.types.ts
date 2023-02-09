6afaed04ddbb1e38ad279410b99bbfcb5e7e916c
박영철 <parkdigy@gmail.com>
2023-02-09 14:23:47 +0900

    Update DefaultLayout.types.ts

--- examples/src/layout/DefaultLayout/DefaultLayout.types.ts
+++ examples/src/layout/DefaultLayout/DefaultLayout.types.ts
@@ -1,7 +1,7 @@
 import { createTheme, LinkProps } from '@mui/material';
 import LinkBehavior from './LinkBehavior';
 
-export const theme = createTheme({
+let theme = createTheme({
   typography: {
     fontFamily: [
       '-apple-system',
@@ -48,3 +48,58 @@ export const theme = createTheme({
     },
   },
 });
+
+const dialogPaddingY = 10;
+theme = {
+  ...theme,
+  components: {
+    ...theme.components,
+    MuiDialog: {
+      styleOverrides: {
+        root: {
+          '&.color-primary .MuiDialogTitle-root': {
+            backgroundColor: theme.palette['primary'].main,
+            color: theme.palette['primary'].contrastText,
+            paddingTop: dialogPaddingY,
+            paddingBottom: dialogPaddingY,
+          },
+          '&.color-secondary .MuiDialogTitle-root': {
+            backgroundColor: theme.palette['secondary'].main,
+            color: theme.palette['secondary'].contrastText,
+            paddingTop: dialogPaddingY,
+            paddingBottom: dialogPaddingY,
+          },
+          '&.color-error .MuiDialogTitle-root': {
+            backgroundColor: theme.palette['error'].main,
+            color: theme.palette['error'].contrastText,
+            paddingTop: dialogPaddingY,
+            paddingBottom: dialogPaddingY,
+          },
+          '&.color-info .MuiDialogTitle-root': {
+            backgroundColor: theme.palette['info'].main,
+            color: theme.palette['info'].contrastText,
+            paddingTop: dialogPaddingY,
+            paddingBottom: dialogPaddingY,
+          },
+          '&.color-success .MuiDialogTitle-root': {
+            backgroundColor: theme.palette['success'].main,
+            color: theme.palette['success'].contrastText,
+            paddingTop: dialogPaddingY,
+            paddingBottom: dialogPaddingY,
+          },
+          '&.color-warning .MuiDialogTitle-root': {
+            backgroundColor: theme.palette['warning'].main,
+            color: theme.palette['warning'].contrastText,
+            paddingTop: dialogPaddingY,
+            paddingBottom: dialogPaddingY,
+          },
+          '.MuiDialogTitle-root + .MuiDialogContent-root': {
+            paddingTop: 24,
+          },
+        },
+      },
+    },
+  },
+};
+
+export { theme };
