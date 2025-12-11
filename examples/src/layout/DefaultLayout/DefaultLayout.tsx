be3560ea39fb32e9b341bcb314f213572b714ce7
parkdigy <parkdigy@gmail.com>
2025-12-11 18:33:45 +0900

    Update

--- examples/src/layout/DefaultLayout/DefaultLayout.tsx
+++ examples/src/layout/DefaultLayout/DefaultLayout.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import * as AdminLayout from '@pdg/react-admin-layout';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import menu from './menu.json';
+import Menu from './menu.json';
 import { theme } from './DefaultLayout.types';
 import MainRouter from '../../router';
+
+const menu = Menu.map((info) => ({
+  ...info,
+  uri: !info.uri ? info.uri : env.isProduction ? `/${env.name}${info.uri}` : info.uri,
+  items: info.items?.map((subInfo) => ({
+    ...subInfo,
+    uri: !subInfo.uri ? subInfo.uri : env.isProduction ? `/${env.name}${subInfo.uri}` : subInfo.uri,
+  })),
+}));
 
 const DefaultLayout = () => {
   //--------------------------------------------------------------------------------------------------------------------
