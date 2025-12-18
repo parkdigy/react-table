a83dc8d0f2ee5280db7ba750f4a6c3a7d7cfdc62
parkdigy <parkdigy@gmail.com>
2025-12-18 17:18:29 +0900

    Update

--- examples/src/layout/DefaultLayout/LinkBehavior.tsx
+++ examples/src/layout/DefaultLayout/LinkBehavior.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router';
 
-const LinkBehavior = React.forwardRef<HTMLAnchorElement, Omit<RouterLinkProps, 'to'> & { href: RouterLinkProps['to'] }>(
-  (props, ref) => {
-    const { href, ...other } = props;
-    return <RouterLink ref={ref} data-testid='custom-link' to={href} {...other} />;
-  }
-);
-
-LinkBehavior.displayName = 'LinkBehavior';
+const LinkBehavior = ({
+  href,
+  ...props
+}: Omit<RouterLinkProps, 'to'> & { href: RouterLinkProps['to'] } & {
+  ref?: React.Ref<HTMLAnchorElement>;
+}) => {
+  return <RouterLink data-testid='custom-link' to={href} {...props} />;
+};
 
 export default LinkBehavior;
